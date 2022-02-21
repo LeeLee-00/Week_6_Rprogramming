3367706edcb68324919d3a1275a43e44058f060e
Lee Joey Noel <70347547+LeeLee-00@users.noreply.github.com>
2022-02-20 22:25:25 -0500

    Add files via upload

---
+++ weekAssignment6.R
@@ -0,0 +1,28 @@
+# Provided Matrix Data.
+A = matrix(c(2,0,1,3), ncol=2)
+B = matrix(c(5,2,4,-1), ncol=2)
+
+
+
+# part a
+A + B
+
+# part b
+A - B
+
+#Adding Matrix A and B
+x = A + B
+x
+
+#Adding Matrix A and B
+y = A - B
+y
+
+# Part C code
+diag(c(4,1,2,3))
+
+
+# Part D Code.
+rbind(c(3, 1, 1, 1, 1), 
+cbind(c(2,2,2,2), 
+diag(3, nrow = 4)))
